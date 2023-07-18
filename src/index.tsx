11b7ffd60c72fe2ff09254361df204d8ccc6c916
alreadynyeong <kjw87130@naver.com>
2023-07-18 14:09:11 +0900

    #6 Feat: 디자인시스템 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/color";
-import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/Color';
+import reportWebVitals from './reportWebVitals';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+import GlobalStyles from './styles/GlobalStyles';
+import './fonts/Font.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <ThemeProvider theme={theme}>
-  <React.StrictMode>
-    <GlobalStyle/>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
   </ThemeProvider>
 );
 
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,8 +1,5 @@
-import { createGlobalStyle } from "styled-components";
-import PretendardBold from 'src/fonts/PretendardBold.woff2';
-import PretendardMedium from 'src/fonts/PretendardMedium.woff2';
-import PretendardLight from 'src/fonts/PretendardLight.woff2';
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
@@ -11,7 +8,9 @@ const GlobalStyles = createGlobalStyle`
         background-color: #ffffff;
     }
     html,
-    body,
+    body {
+        font-family: 'Pretendard';
+    },
     div,
     span,
     h1,
@@ -36,103 +35,6 @@ const GlobalStyles = createGlobalStyle`
         border: 0;
         vertical-align: baseline;
     }
-
-    @font-face {
-        font-family: 'H0';
-        font-style: normal;
-        font-size: 40px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H1';
-        font-style: normal;
-        font-size: 35px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H2';
-        font-style: normal;
-        font-size: 30px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H3';
-        font-style: normal;
-        font-size: 28px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H4';
-        font-style: normal;
-        font-size: 26px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H5';
-        font-style: normal;
-        font-size: 20px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'H6';
-        font-style: normal;
-        font-size: 17px;
-        src: url("src/fonts/PretendardBold.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'M0';
-        font-style: normal;
-        font-size: 55px;
-        src: url("src/fonts/PretendardMedium.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'M1';
-        font-style: normal;
-        font-size: 22px;
-        src: url("src/fonts/PretendardMedium.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'M2';
-        font-style: normal;
-        font-size: 14px;
-        src: url("src/fonts/PretendardMedium.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L0';
-        font-style: normal;
-        font-size: 50px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L1';
-        font-style: normal;
-        font-size: 25px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L2';
-        font-style: normal;
-        font-size: 20px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L3';
-        font-style: normal;
-        font-size: 18px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L4';
-        font-style: normal;
-        font-size: 16px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
-    @font-face {
-        font-family: 'L5';
-        font-style: normal;
-        font-size: 14px;
-        src: url("src/fonts/PretendardLight.woff2") format("woff2");
-    }
 `;
- 
+
 export default GlobalStyles;
