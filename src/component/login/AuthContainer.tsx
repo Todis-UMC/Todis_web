511b5ac36c922add1a14a4dca93639880af85b37
alreadynyeong <kjw87130@naver.com>
2023-07-22 12:51:37 +0900

    #19 Feat: 컨테이너 전체를 컴포넌트로 변경

--- src/component/login/AuthContainer.tsx
+++ src/component/login/AuthContainer.tsx
@@ -1,6 +1,29 @@
 import styled from 'styled-components';
+import { ReactComponent as SmallLogo } from '../../assets/icon/SmallLogo.svg';
+import React from 'react';
+import FONT from '../../styles/Font';
 
-export const Container = styled.div`
+const AuthContainer = ({
+  title,
+  component
+}: {
+  title: string;
+  component: React.ReactElement;
+}) => {
+  return (
+    <Container>
+      <Box>
+        <SmallLogo />
+        <Title style={FONT.H5}>{title}</Title>
+        {component}
+      </Box>
+    </Container>
+  );
+};
+
+export default AuthContainer;
+
+const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-color: ${(props) => props.theme.Sky_Blue_04};
@@ -8,8 +31,7 @@ export const Container = styled.div`
   justify-content: center;
   display: flex;
 `;
-
-export const Box = styled.div`
+const Box = styled.div`
   max-width: 569px;
   width: 80%;
   height: 730px;
@@ -18,4 +40,8 @@ export const Box = styled.div`
   justify-content: center;
   padding: 44px 65px 50px 65px;
   text-align: center;
+`;
+const Title = styled.div`
+  margin-top: 19px;
+  margin-bottom: 42px;
 `;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,12 @@ import FONT from '../styles/Font';
 import { ReactComponent as SmallUnCheck } from '../assets/icon/SmallUnCheck.svg';
 import { ReactComponent as SmallCheck } from '../assets/icon/SmallCheck.svg';
 import { LoginProps } from '../types/Auth';
-import { Container, Box } from '../component/login/AuthContainer';
+import AuthContainer from '../component/login/AuthContainer';
+
+const LoginPage = () => <AuthContainer title='로그인' component={<Login />} />;
+
+export default LoginPage;
+
 const Login = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -37,54 +42,44 @@ const Login = () => {
     console.log(login);
   };
   return (
-    <Container>
-      <Box>
-        <SmallLogo />
-        <Title style={FONT.H5}>로그인</Title>
-        <Input
-          label='이메일'
-          type='email'
-          placeholder='이메일 주소 입력'
-          value={email}
-          onChange={(ev) => setEmail(ev.target.value)}
-        />
-        <div style={{ height: 15 }} />
-        <Input
-          label='비밀번호'
-          type='password'
-          placeholder='비밀번호 입력'
-          value={password}
-          onChange={(ev) => setPassword(ev.target.value)}
-        />
-        <Button onClick={() => handleLoginBtn()}>로그인</Button>
-        <Setting>
-          <div style={FONT.L6} onClick={() => setMemory(!memory)}>
-            {memory ? <SmallCheck /> : <SmallUnCheck />}
-            로그인 정보 기억하기
-          </div>
-          <div>
-            <span style={FONT.L6}>아이디 찾기</span>
-            <span style={FONT.L6}> | </span>
-            <span style={FONT.L6}>비밀번호 찾기</span>
-          </div>
-        </Setting>
-        <SocialGoogle />
-        <SocialKakao />
-        <SignUp style={FONT.L6}>
-          계정이 없으신가요?
-          <a href='/signup'> 회원가입</a>
-        </SignUp>
-      </Box>
-    </Container>
+    <>
+      <Input
+        label='이메일'
+        type='email'
+        placeholder='이메일 주소 입력'
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
+      />
+      <div style={{ height: 15 }} />
+      <Input
+        label='비밀번호'
+        type='password'
+        placeholder='비밀번호 입력'
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <Button onClick={() => handleLoginBtn()}>로그인</Button>
+      <Setting>
+        <div style={FONT.L6} onClick={() => setMemory(!memory)}>
+          {memory ? <SmallCheck /> : <SmallUnCheck />}
+          로그인 정보 기억하기
+        </div>
+        <div>
+          <span style={FONT.L6}>아이디 찾기</span>
+          <span style={FONT.L6}> | </span>
+          <span style={FONT.L6}>비밀번호 찾기</span>
+        </div>
+      </Setting>
+      <SocialGoogle />
+      <SocialKakao />
+      <SignUp style={FONT.L6}>
+        계정이 없으신가요?
+        <a href='/signup'> 회원가입</a>
+      </SignUp>
+    </>
   );
 };
 
-export default Login;
-
-const Title = styled.div`
-  margin-top: 19px;
-  margin-bottom: 42px;
-`;
 const SignUp = styled.div`
   margin-top: 22px;
   color: ${(props) => props.theme.Typo_Black};
