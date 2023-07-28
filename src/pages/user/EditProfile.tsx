4ca8942fe9d57fc256f2ff26620b14123d428416
alreadynyeong <kjw87130@naver.com>
2023-07-28 12:54:09 +0900

    #37 Feat: 비밀번호 변경 버튼 생성

--- src/pages/user/EditProfile.tsx
+++ src/pages/user/EditProfile.tsx
@@ -38,13 +38,16 @@ const EditProfile: React.FC<{ user: UserProps }> = ({ user }) => {
       <div style={{ height: 17 }} />
       <Input label='아이디' type='email' value={user.email} disabled={true} />
       <div style={{ height: 17 }} />
-      <Input
-        label='비밀번호'
-        type='password'
-        value={password}
-        onChange={(ev) => setPassword(ev.target.value)}
-        placeholder='비밀번호를 입력하세요'
-      />
+      <InputBox>
+        <Input
+          label='비밀번호'
+          type='password'
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          placeholder='비밀번호를 입력하세요'
+        />
+        <ShortButton>변경</ShortButton>
+      </InputBox>
       <Button>수정완료</Button>
       <A href='/user/delete' style={FONT.L6}>
         계정 삭제하기
@@ -68,4 +71,22 @@ const Button = styled.button`
   font-size: 16px;
   font-weight: 500;
   cursor: pointer;
+`;
+const InputBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 6px;
+`;
+const ShortButton = styled.button`
+  width: 30%;
+  height: 55px;
+  border-radius: 14px;
+  border: none;
+  background-color: ${(props) => props.theme.Blue_Main};
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  margin-top: 28px;
 `;
