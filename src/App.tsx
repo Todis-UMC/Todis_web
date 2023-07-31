25bc4722f51145c16cf5b018e218cdf6d5a78315
JAin <125685354+4928vibe@users.noreply.github.com>
2023-07-31 16:52:33 +0900

    #33 NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,22 @@ import Login from './pages/Login';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Friend from './pages/Friend';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './component/Nav/Nav';
+import Nav from './component/common/Nav/Nav';
+import NavLogin from './component/common/Nav/NavLogin';
+import SignUpBeforePage from './pages/signup/SignUpBefore';
 
 function App() {
-  const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-
-  // client_id가 설정되어 있는지 확인
-  if (!client_id) {
-    console.error(
-      'Google Client ID가 설정되지 않았습니다. .env 파일에 REACT_APP_GOOGLE_CLIENT_ID를 설정하세요.'
-    );
-    return null;
-  }
 
   return (
     <BrowserRouter>
       <div className='App'>
-        <GoogleOAuthProvider clientId={client_id}>
           <Nav />
           <Routes>
-            <Route path='/Friend' element={<Friend />}/>
+            <Route path='/Friend' element={<Friend />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/SignUp' element={<SignUpBeforePage />} />
+            <Route path='/Nav' element={<Nav />} />
           </Routes>
-          <Login />
-        </GoogleOAuthProvider>
       </div>
     </BrowserRouter>
 
---
+++ src/component/common/Nav/DropDownButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DropDownButton: React.FC = () => {
+  return (
+    <>
+      <li>회원정보 수정</li>
+      <li>로그아웃</li>
+    </>
+  );
+};
+
+export default DropDownButton;
