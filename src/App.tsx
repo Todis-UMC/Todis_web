156b359f5adce51a18b95062e905a8a6e48b656d
alreadynyeong <kjw87130@naver.com>
2023-07-31 17:33:29 +0900

    #33 Fix: 코드 누락으로 재추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,29 @@ import SignUpBeforePage from './pages/signup/SignUpBefore';
 import SignUpEmailPage from './pages/signup/SignUpEmail';
 
 function App() {
+  const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+  // client_id가 설정되어 있는지 확인
+  if (!client_id) {
+    console.error(
+      'Google Client ID가 설정되지 않았습니다. .env 파일에 REACT_APP_GOOGLE_CLIENT_ID를 설정하세요.'
+    );
+    return null;
+  }
 
   return (
     <>
       <BrowserRouter>
-      <GoogleOAuthProvider clientId={client_id}>
-        <div className='App'>
-          <Routes>
-            <Route path='/Friend' element={<Friend />} />
-            <Route path='/Login' element={<Login />} />
-            <Route path='/SignUp' element={<SignUpBeforePage />} />
-            <Route path='/Nav' element={<Nav />} />
-          </Routes>
-        </div>
-      </GoogleOAuthProvider>
+        <GoogleOAuthProvider clientId={client_id}>
+          <div className='App'>
+            <Routes>
+              <Route path='/Friend' element={<Friend />} />
+              <Route path='/Login' element={<Login />} />
+              <Route path='/SignUp' element={<SignUpBeforePage />} />
+              <Route path='/Nav' element={<Nav />} />
+            </Routes>
+          </div>
+        </GoogleOAuthProvider>
       </BrowserRouter>
     </>
   );
