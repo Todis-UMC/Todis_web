f1230b8d8748b092390912ee5fd015f2fcdae682
alreadynyeong <kjw87130@naver.com>
2023-08-10 22:23:23 +0900

    #54 Refactor: baseAPI 변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,92 +1,53 @@
 const baseURL =
   'http://ec2-13-209-15-210.ap-northeast-2.compute.amazonaws.com:8080';
 
-const token = localStorage.getItem('token');
-
 const baseHeaders = {
   'Content-Type': 'application/json'
 };
+
 const getHeadersWithToken = () => {
   const token = localStorage.getItem('token');
   return token
     ? { ...baseHeaders, Authorization: `Bearer ${token}` }
     : { ...baseHeaders };
 };
-interface Request {
+
+interface RequestConfig {
   url: string;
-  headers?: object;
+  headers?: Record<string, string>;
   method: 'get' | 'post' | 'delete' | 'patch' | 'put';
+  params?: object;
+  data?: object;
 }
 
-interface RequestWithParams extends Request {
-  method: 'get' | 'delete';
-  params: object;
-}
-
-interface RequestWithData extends Request {
-  method: 'post' | 'patch' | 'put';
-  data: object;
-}
-
-const sendRequest = async ({ url, headers, method }: RequestWithParams) => {
-  return fetch(baseURL + url, {
-    method,
-    headers: { ...baseHeaders, ...headers }
-  }).then((res) => res.json());
-};
-
-const sendRequestWithData = async ({
+const sendAuthorizedRequest = async ({
   url,
   headers,
   method,
   data
-}: RequestWithData) => {
-  return fetch(baseURL + url, {
+}: RequestConfig) => {
+  const requestOptions: RequestInit = {
     method,
-    headers: { ...baseHeaders, ...headers },
-    body: JSON.stringify(data)
-  }).then((res) => res.json());
+    headers: { ...getHeadersWithToken(), ...headers }
+  };
+
+  if (data) {
+    requestOptions.body = JSON.stringify(data);
+  }
+
+  const response = await fetch(baseURL + url, requestOptions);
+  return response.json();
 };
 
+const createRequestMethod =
+  (method: RequestConfig['method']) =>
+  async (requestConfig: Omit<RequestConfig, 'method'>) =>
+    sendAuthorizedRequest({ ...requestConfig, method });
+
 export const request = {
-  get: ({ url, params, headers }: Omit<RequestWithParams, 'method'>) => {
-    return sendRequest({
-      url,
-      params,
-      headers: { ...getHeadersWithToken(), ...headers },
-      method: 'get'
-    });
-  },
-  post: ({ url, data, headers }: Omit<RequestWithData, 'method'>) => {
-    return sendRequestWithData({
-      url,
-      data,
-      headers: { ...getHeadersWithToken(), ...headers },
-      method: 'post'
-    });
-  },
-  put: ({ url, data, headers }: Omit<RequestWithData, 'method'>) => {
-    return sendRequestWithData({
-      url,
-      data,
-      headers: { ...getHeadersWithToken(), ...headers },
-      method: 'put'
-    });
-  },
-  patch: ({ url, data, headers }: Omit<RequestWithData, 'method'>) => {
-    return sendRequestWithData({
-      url,
-      data,
-      headers: { ...getHeadersWithToken(), ...headers },
-      method: 'patch'
-    });
-  },
-  delete: ({ url, params, headers }: Omit<RequestWithParams, 'method'>) => {
-    return sendRequest({
-      url,
-      params,
-      headers: { ...getHeadersWithToken(), ...headers },
-      method: 'delete'
-    });
-  }
+  get: createRequestMethod('get'),
+  post: createRequestMethod('post'),
+  put: createRequestMethod('put'),
+  patch: createRequestMethod('patch'),
+  delete: createRequestMethod('delete')
 };
