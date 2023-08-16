dc41a9aa28f5ba9128a43edd4091e70a64a79dbf
JAin <125685354+4928vibe@users.noreply.github.com>
2023-08-17 05:48:32 +0900

    #59 푸터 수정

--- src/component/common/Footer/Footer.tsx
+++ src/component/common/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const FooterContainer = styled.footer`
   text-align: center;
   display: flex;
   flex-direction: column;
-  height: 38vh;
+  height: 30vh;
 `;
 
 const Message = styled.div`
@@ -18,16 +18,6 @@ const Message = styled.div`
 `;
 
 const Copyright = styled.div`
-  color: #181818;
-  margin-bottom: 10px;
-`;
-
-const Number = styled.div`
-  color: #181818;
-  margin-bottom: 10px;
-`;
-
-const Information = styled.div`
   color: #181818;
   margin-bottom: 10px;
 `;
@@ -58,11 +48,9 @@ const Footer: React.FC = () => {
             <Link to="/terms/1" style={{ color: 'inherit', textDecoration: 'none' }}>
               개인정보처리방침
             </Link>
-            {' | 고객센터 | @TODIS Corp.'}
+            {' | @TODIS Corp.'}
         </Message>
         <Copyright>저작권 ⓒ Todis. All rights reserved</Copyright>
-        <Number>사업자 등록 번호 : 123-12-12345  대표전화 : 1234-5678  팩스 : 02-123-4567</Number>
-        <Information>대표자 : 김이름  대표메일 : Todayis@weather.co.kr  서울특별시 도봉구 삼양로 144길 33 TODIS 2023, 1234</Information>
       </div>
       </FooterWrapper>
     </FooterContainer>
