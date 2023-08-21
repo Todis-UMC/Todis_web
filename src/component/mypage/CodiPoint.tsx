1f6194fcaba6a75403566ab0808931d2e00f9ed4
nagyeongyun <skruddbs99@naver.com>
2023-08-21 19:56:41 +0900

    #70 Fix: 위치 수정(서울 고정)

--- src/component/mypage/CodiPoint.tsx
+++ src/component/mypage/CodiPoint.tsx
@@ -42,120 +42,112 @@ const CodiPoint = () => {
   ]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      async function (position) {
-        try {
-          const [weatherResponse, airQualityResponse, uviResponse] =
-            await Promise.all([
-              fetch(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=4d4c41dc06bbf1741b3a628d64934b98&lang=kr`
-              ),
-              fetch(
-                `http://api.openweathermap.org/data/2.5/air_pollution?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=4d4c41dc06bbf1741b3a628d64934b98&lang=kr`
-              ),
-              fetch(
-                `http://api.openweathermap.org/data/2.5/uvi?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=4d4c41dc06bbf1741b3a628d64934b98`
-              )
-            ]);
-
-          if (
-            !weatherResponse.ok ||
-            !airQualityResponse.ok ||
-            !uviResponse.ok
-          ) {
-            throw new Error('One or more API responses were not ok');
-          }
-
-          const weatherData = await weatherResponse.json();
-          const airQualityData = await airQualityResponse.json();
-          const uviData = await uviResponse.json();
-
-          const temp = weatherData.main.temp; // 기온
-          const humidity = weatherData.main.humidity; // 습도
-          const forecastData = weatherData.weather[0].main; // Rain, Snow
-          const airQuality = airQualityData.list[0].main.aqi; // 대기질
-          const uvi = uviData.value; // 자외선
-          console.log(
-            '기온:',
-            temp,
-            '습도:',
-            humidity,
-            '예측:',
-            forecastData,
-            '대기질:',
-            airQuality,
-            '자외선:',
-            uvi
-          );
-
-          let FirstText = '';
-          let SecondText = '';
-          let ThirdText = '';
-          // 1. 기온
-          for (const range of TempRanges) {
-            if (temp >= range.min && temp <= range.max) {
-              const randomIndex = Math.floor(Math.random() * range.text.length);
-              FirstText = range.text[randomIndex];
-              break;
-            }
-          }
-          // 2. 습도, Rain, Snow
-          if (
-            humidity >= 50 ||
-            forecastData === 'Rain' ||
-            forecastData === 'Snow'
-          ) {
-            const randomHumIndex = Math.floor(
-              Math.random() * HumidityData.length
-            );
-            const randomRainIndex = Math.floor(Math.random() * 4);
-            const randomSnowIndex = Math.floor(Math.random() * 2) + 4;
-            SecondText =
-              humidity >= 50
-                ? HumidityData[randomHumIndex]
-                : forecastData === 'Rain'
-                ? ForecastData[randomRainIndex]
-                : ForecastData[randomSnowIndex];
-          }
-          // 3. 자외선, 대기질
-          if (uvi >= 6 || airQuality >= 150) {
-            const randomUviIndex = Math.floor(
-              Math.random() * UviTextData.length
-            );
-            const randomAirIndex = Math.floor(Math.random() * 2);
-            ThirdText =
-              uvi >= 6
-                ? UviTextData[randomUviIndex]
-                : AirTextData[randomAirIndex];
-          } else {
-            ThirdText = AirTextData[2];
-          }
-
-          setCoditexts((prevCoditexts) => [
-            {
-              id: 1,
-              text: FirstText,
-              isChecked: false
-            },
-            {
-              id: 2,
-              text: SecondText,
-              isChecked: false
-            },
-            {
-              id: 3,
-              text: ThirdText,
-              isChecked: false
-            }
+    const fetchData = async () => {
+      const latitude = 37;
+      const longitude = 126;
+      try {
+        const [weatherResponse, airQualityResponse, uviResponse] =
+          await Promise.all([
+            fetch(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=4d4c41dc06bbf1741b3a628d64934b98&lang=kr`
+            ),
+            fetch(
+              `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=4d4c41dc06bbf1741b3a628d64934b98&lang=kr`
+            ),
+            fetch(
+              `http://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=4d4c41dc06bbf1741b3a628d64934b98`
+            )
           ]);
-        } catch (error) {
-          console.error(error);
+
+        if (!weatherResponse.ok || !airQualityResponse.ok || !uviResponse.ok) {
+          throw new Error('One or more API responses were not ok');
         }
-      },
-      function (error) {
+
+        const weatherData = await weatherResponse.json();
+        const airQualityData = await airQualityResponse.json();
+        const uviData = await uviResponse.json();
+
+        const temp = weatherData.main.temp; // 기온
+        const humidity = weatherData.main.humidity; // 습도
+        const forecastData = weatherData.weather[0].main; // Rain, Snow
+        const airQuality = airQualityData.list[0].main.aqi; // 대기질
+        const uvi = uviData.value; // 자외선
+        console.log(
+          '기온:',
+          temp,
+          '습도:',
+          humidity,
+          '예측:',
+          forecastData,
+          '대기질:',
+          airQuality,
+          '자외선:',
+          uvi
+        );
+
+        let FirstText = '';
+        let SecondText = '';
+        let ThirdText = '';
+        // 1. 기온
+        for (const range of TempRanges) {
+          if (temp >= range.min && temp <= range.max) {
+            const randomIndex = Math.floor(Math.random() * range.text.length);
+            FirstText = range.text[randomIndex];
+            break;
+          }
+        }
+        // 2. 습도, Rain, Snow
+        if (
+          humidity >= 50 ||
+          forecastData === 'Rain' ||
+          forecastData === 'Snow'
+        ) {
+          const randomHumIndex = Math.floor(
+            Math.random() * HumidityData.length
+          );
+          const randomRainIndex = Math.floor(Math.random() * 4);
+          const randomSnowIndex = Math.floor(Math.random() * 2) + 4;
+          SecondText =
+            humidity >= 50
+              ? HumidityData[randomHumIndex]
+              : forecastData === 'Rain'
+              ? ForecastData[randomRainIndex]
+              : ForecastData[randomSnowIndex];
+        }
+        // 3. 자외선, 대기질
+        if (uvi >= 6 || airQuality >= 150) {
+          const randomUviIndex = Math.floor(Math.random() * UviTextData.length);
+          const randomAirIndex = Math.floor(Math.random() * 2);
+          ThirdText =
+            uvi >= 6
+              ? UviTextData[randomUviIndex]
+              : AirTextData[randomAirIndex];
+        } else {
+          ThirdText = AirTextData[2];
+        }
+
+        setCoditexts((prevCoditexts) => [
+          {
+            id: 1,
+            text: FirstText,
+            isChecked: false
+          },
+          {
+            id: 2,
+            text: SecondText,
+            isChecked: false
+          },
+          {
+            id: 3,
+            text: ThirdText,
+            isChecked: false
+          }
+        ]);
+      } catch (error) {
         console.error(error);
       }
-    );
+    };
+    fetchData();
   }, []);
 
   const CodiCheckHandler = (id: number) => {
