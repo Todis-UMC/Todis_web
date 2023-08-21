3662587e102867e1f109b8ad4d0b65996a7f40fe
JAin <125685354+4928vibe@users.noreply.github.com>
2023-08-21 23:53:45 +0900

    #58 Fix: LankBox 오류 수정

--- src/pages/Lank.tsx
+++ src/pages/Lank.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import FONT from '../styles/Font';
 import LankBox from '../component/Lank/LankBox';
